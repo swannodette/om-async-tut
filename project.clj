debbe27b281cf7ec168cffa6725c25de382e43ca
David Nolen <david.nolen@gmail.com>
2014-02-15 12:07:33 -0500

    0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-async-tut/lein-template "0.1.3"
+(defproject om-async-tut/lein-template "0.1.4"
   :description "Om async tutorial"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/om_async_tut/project.clj
+++ src/leiningen/new/om_async_tut/project.clj
@@ -10,7 +10,7 @@
                  [org.clojure/clojurescript "0.0-2156"]
                  [ring/ring "1.2.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.4.0"]
+                 [om "0.4.1"]
                  [compojure "1.1.6"]
                  [fogus/ring-edn "0.2.0"]
                  [com.datomic/datomic-free "0.9.4532"]]
