39dfce0234c266cb717d10ff2a6a873ffb80d1a5
David Nolen <david.nolen@gmail.com>
2014-02-16 18:35:51 -0500

    om 0.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-async-tut/lein-template "0.1.4"
+(defproject om-async-tut/lein-template "0.1.5"
   :description "Om async tutorial"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse Public License"
