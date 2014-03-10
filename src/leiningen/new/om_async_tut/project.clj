3f5548dba0588ba44901341984e09d4477424523
mynomoto <mynomoto@gmail.com>
2014-03-10 13:59:21 -0300

    Update versions of clojurescript, datomic and om

--- src/leiningen/new/om_async_tut/project.clj
+++ src/leiningen/new/om_async_tut/project.clj
@@ -7,13 +7,13 @@
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [ring/ring "1.2.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0"]
+                 [om "0.5.2"]
                  [compojure "1.1.6"]
                  [fogus/ring-edn "0.2.0"]
-                 [com.datomic/datomic-free "0.9.4532"]]
+                 [com.datomic/datomic-free "0.9.4578"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
