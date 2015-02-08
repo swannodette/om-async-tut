ca8222563f30a3dcd08452fcb3bee510aeea7126
Sebastian Bensusan <sbensu@gmail.com>
2015-02-08 13:45:03 +0100

    Updated project.clj to new dependencies, plugins, and clean targets

--- src/leiningen/new/om_async_tut/project.clj
+++ src/leiningen/new/om_async_tut/project.clj
@@ -6,25 +6,31 @@
 
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [ring/ring "1.2.1"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.3"]
-                 [compojure "1.1.6"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2727"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.omcljs/om "0.8.7"]
+                 [ring "1.3.2"]
+                 [compojure "1.3.1"]
+                 [figwheel "0.2.2-SNAPSHOT"]
                  [fogus/ring-edn "0.2.0"]
-                 [com.datomic/datomic-free "0.9.4699"]]
+                 [com.datomic/datomic-free "0.9.5130" :exclusions [joda-time]]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [lein-ring "0.9.1"]
+            [lein-figwheel "0.2.2-SNAPSHOT"]]
+
 
   :source-paths ["src/clj" "src/cljs"]
   :resource-paths ["resources"]
+  :clean-targets ^{:protect false} ["resources/public/js/out"
+                                    "resources/public/js/main.js"]
 
   :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src/clj" "src/cljs"]
-              :compiler {
-                :output-to "resources/public/js/main.js"
-                :output-dir "resources/public/js/out"
-                :optimizations :none
-                :source-map true}}]})
+              :builds [{:id "dev"
+                        :source-paths ["src/clj" "src/cljs"]
+                        :compiler {
+                                   :output-to "resources/public/js/main.js"
+                                   :output-dir "resources/public/js/out"
+                                   :optimizations :none
+                                   :source-map true}}]})
